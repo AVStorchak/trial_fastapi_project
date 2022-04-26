64f0043c997aa3fe62545bc3501f6ede0a51d816
Stanislav.Zaynullin <Stanislav.Zaynullin@nedra.digital>
2022-04-26 16:59:22 +0300

    update consumer add env to compose

--- rabbit_consumer/consumer.py
+++ rabbit_consumer/consumer.py
@@ -1,24 +1,26 @@
 import pika, sys, os
 
-# host = os.environ.get('AMQP_HOST')
-# queue = os.environ.get('QUEUE_NAME')
-host = "localhost"
-queue = "hello"
+host = os.environ.get("AMQP_HOST")
+queue = os.environ.get("QUEUE_NAME")
 
 
 def main():
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body.decode())
 
-    connection_params = pika.ConnectionParameters(host=host)
-    connection = pika.BlockingConnection(connection_params)
-    channel = connection.channel()
-
-    channel.queue_declare(queue=queue)
-    channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
-
-    print("Subscribed to " + queue + ", waiting for messages...")
-    channel.start_consuming()
+    creds = pika.PlainCredentials("rabbit", "rabbit")
+    connection_params = pika.ConnectionParameters(host, 5672, "/", creds, )
+    try:
+        connection = pika.BlockingConnection(connection_params)
+    except Exception as e:
+        print(f"Exception occurred: {e}")
+        sys.exit(1)
+    else:
+        channel = connection.channel()
+        channel.queue_declare(queue=queue)
+        channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
+        print("Subscribed to " + queue + ", waiting for messages...")
+        channel.start_consuming()
 
 
 if __name__ == "__main__":
