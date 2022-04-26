ece8810327def42688330aae422fd39ff5f11fca
Stanislav.Zaynullin <Stanislav.Zaynullin@nedra.digital>
2022-04-26 16:42:00 +0300

    add Plain credentioals

--- rabbit_producer/producer.py
+++ rabbit_producer/producer.py
@@ -12,13 +12,14 @@ queue = "hello"
 
 @router.post("/publish-message/")
 def publish_message(message: str):
-    connection_params = pika.ConnectionParameters(host=host)
+    creds = pika.PlainCredentials("rabbit", "rabbit")
+    connection_params = pika.ConnectionParameters(host, 5672, "/", creds, )
     connection = pika.BlockingConnection(connection_params)
     channel = connection.channel()
     channel.queue_declare(queue=queue)
     channel.basic_publish(exchange="",
-                          routing_key="hello",
-                          body=message)
+                          routing_key=queue,
+                          body=bytes(message, "utf-8"))
     print(f" [x] Sent {message}")
     connection.close()
     return {"info": "message sent successfully"}
